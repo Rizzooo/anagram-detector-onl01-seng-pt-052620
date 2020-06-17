af3ab03c16b116012ce0583431ba51d2fb679d52
Ronald Burrell Jr <ronburrell05@gmail.com>
2020-06-17 18:12:05 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,7 +7,7 @@ class Anagram
     @word = word
   end
   
-  def self.match(word_array)
+  def match(word_array)
     word_array.select do |word|
       word.split("").sort == @word.split("").sort
     end
